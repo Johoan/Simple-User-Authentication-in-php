87a1637e9b5d154a572205f296506ad51fa2f1b2
Johoan <38538750+Johoan@users.noreply.github.com>
2021-04-14 23:45:16 +0100

    Code processing the entries during registration

---
+++ formdata.php
@@ -0,0 +1,34 @@
+<?php
+
+session_start();
+
+if(isset($_POST['submit'])){
+    $firstname = $_POST['firstname'];
+    $lastname = $_POST['lastname'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    
+    
+    
+    
+    
+    $info_array = [
+
+        'firstname' => $firstname,
+        'lastname' => $lastname,
+        'email' => $email,
+        'password' => $password,
+         
+    ];
+
+    echo "Welcome " . $_POST['firstname']. "<br><br>";
+    echo "Your email address is " . $_POST['email'];
+
+    file_put_contents("users.txt", json_encode($info_array)."\r\n", FILE_APPEND);
+
+}
+
+
+ 
+?>
+
