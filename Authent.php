7dcdb08a7ac7c61a149448bfb23bbb6024bd387b
Johoan <38538750+Johoan@users.noreply.github.com>
2021-04-14 23:43:31 +0100

    Registration page

---
+++ Authent.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+
+<html>
+
+    <head>
+
+    </head>
+
+
+    <body>
+
+        <form action="formdata.php" method="post">
+
+            <fieldset>
+
+            <legend>Register</legend>
+        
+            First Name: <input type="text" name="firstname" required><br><br>
+            Last Name: <input type="text" name="lastname" required><br><br>
+            E-mail: <input type="email" name="email" required><br><br>
+            Password: <input type="password" name="password" required><br><br>
+            <button type="submit" name="submit">Submit</button>
+
+            </fieldset>
+        </form>
+
+        <br><br><br>
+
+        
+
+    </body>
+
+</html>
