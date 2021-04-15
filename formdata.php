cfc043a4a3e151818b3fbac4c81df029b7906132
Johoan <38538750+Johoan@users.noreply.github.com>
2021-04-15 13:05:43 +0100

    Update

--- formdata.php
+++ formdata.php
@@ -22,7 +22,8 @@ if(isset($_POST['submit'])){
     ];
 
     echo "Welcome " . $_POST['firstname']. "<br><br>";
-    echo "Your email address is " . $_POST['email'];
+    echo "Your email address is " . $_POST['email'] . "<br><br>";
+    echo '<button><a href="login.php" style="text-decoration: none;">Login</a></button>';
 
     file_put_contents("users.txt", json_encode($info_array)."\r\n", FILE_APPEND);
 
