9abf4ddd78827cbbb46736f2477577ce11db502e
Johoan <38538750+Johoan@users.noreply.github.com>
2021-04-15 19:59:42 +0100

    Update home.php

--- home.php
+++ home.php
@@ -13,6 +13,20 @@ session_start();
 
 <button><a href="login.php" style="text-decoration: none;">Logout</a></button>
 
+<?php
+
+    function logout(){
+        session_destroy();
+        header('location:login.php');
+    }
+
+    if(isset($_GET["action"]) && $_GET["action"] == "logout"){
+
+        logout();
+    }
+
+?>
+
 </body>
 
 </html>
