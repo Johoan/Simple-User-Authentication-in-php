f86c12533991e73130ef40101cbdfdf3e97038e0
Johoan <38538750+Johoan@users.noreply.github.com>
2021-04-16 00:19:11 +0100

    Password reset

---
+++ reset.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+
+<html>
+
+<body>
+
+<form>
+
+<input type="email" name="email"><br><br>
+<input type="password" name="password"><br><br>
+<input type="submit" name="submit">
+
+</form>
+
+
+</body>
+
+
+</html>
