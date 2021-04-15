6778ecc730bcd85e8f2b41fc7c29148377104621
Johoan <38538750+Johoan@users.noreply.github.com>
2021-04-16 00:25:31 +0100

    Update reset.php

--- reset.php
+++ reset.php
@@ -6,8 +6,8 @@
 
 <form>
 
-<input type="email" name="email"><br><br>
-<input type="password" name="password"><br><br>
+<input type="email" name="email" placeholder="enter your email"><br><br>
+<input type="password" name="password" placeholder="enter new password"><br><br>
 <input type="submit" name="submit">
 
 </form>
